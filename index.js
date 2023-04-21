4a436ae877f5a98cd5cfab84aa453ec7a1466842
adrianMartinezTeba <adrymate@gmail.com>
2023-04-21 16:45:53 +0200

    conexion con base de datos creada y servidor levantado correctamente

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const { dbConnection } = require("./config/config")
+app.use(express.json())
+dbConnection()
+
+app.listen(PORT, ()=> console.log(`Server started on port ${PORT}`));
