47d71f71b23fd57d785950ecf8f26d8a40b65eb4
adrianMartinezTeba <adrymate@gmail.com>
2023-04-25 13:39:27 +0200

    endpoint actualizar post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,4 +1,4 @@
-const Post = require('../models/Posts');
+const Post = require('../models/Post');
 
 const PostController ={
     async create(req,res){
@@ -10,6 +10,20 @@ const PostController ={
     res.status(500).send({ message: 'Ha habido un problema al crear el post' })
     }
     },
+    async update(req, res) {
+        try {
+          const post = await Post.findByIdAndUpdate(
+            req.params._id,
+            { ...req.body, userId: req.user._id },
+            {
+              new: true,
+            }
+          );
+          res.send({ message: "Post successfully updated", post });
+        } catch (error) {
+          console.error(error);
+        }
+      },
     }
     
     module.exports = PostController;
--- routes/posts.js
+++ routes/posts.js
@@ -5,5 +5,6 @@ const router = express.Router()
 const PostController = require('../controllers/PostController');
 
 router.post('/createPost',PostController.create)
+router.put('/updatePost/:_id',PostController.update)
 
 module.exports = router;
