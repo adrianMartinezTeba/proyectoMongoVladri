6653da5fca0d49fd43856a419ebfbe17e0b2d885
adrianMartinezTeba <adrymate@gmail.com>
2023-04-25 12:15:42 +0200

    modelo post hecho con relacion a modelo User

---
+++ models/Posts.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const PostSchema = new mongoose.Schema({
+
+    title: {
+        type: String,
+        required: true,
+      },
+      content: {
+        type: String,
+        required: true,
+      },
+      user: {
+        type:ObjectId,
+        ref: 'User',
+        required: true,
+      }}, { timestamps: true });
+
+const Post = mongoose.model('Product', PostSchema);
+
+module.exports = Post;
