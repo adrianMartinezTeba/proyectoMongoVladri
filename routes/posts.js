b087f059925900933b65b2fae86c775d5b0d8d58
adrianMartinezTeba <adrymate@gmail.com>
2023-04-26 13:48:18 +0200

    rutas de likes y unñikes

--- routes/posts.js
+++ routes/posts.js
@@ -12,5 +12,7 @@ router.delete('/deletePost/:_id',authentication,isAuthor,PostController.delete)
 router.get('/byId/:_id',PostController.postById)
 router.get('/byName/:name',PostController.postByName)
 router.get('/getAll',PostController.getAllInf)
+router.put('/like',authentication,PostController.like)
+router.put('/unlike',authentication,PostController.unlike)
 
 module.exports = router;
