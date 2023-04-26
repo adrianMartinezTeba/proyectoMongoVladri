8c94dee1e9947b0b5bcaf382d9e2eaaf52b07857
adrianMartinezTeba <adrymate@gmail.com>
2023-04-26 09:14:09 +0200

    endpoint delete post

--- controllers/PostController.js
+++ controllers/PostController.js
@@ -1,29 +1,40 @@
 const Post = require('../models/Post');
 
-const PostController ={
-    async create(req,res){
+const PostController = {
+  async create(req, res) {
     try {
-    const post = await Post.create(req.body)
-    res.status(201).send(post)
+      const post = await Post.create(req.body)
+      res.status(201).send(post)
     } catch (error) {
-    console.error(error)
-    res.status(500).send({ message: 'Ha habido un problema al crear el post' })
+      console.error(error)
+      res.status(500).send({ message: 'Ha habido un problema al crear el post' })
     }
-    },
-    async update(req, res) {
-        try {
-          const post = await Post.findByIdAndUpdate(
-            req.params._id,
-            { ...req.body, userId: req.user._id },
-            {
-              new: true,
-            }
-          );
-          res.send({ message: "Post successfully updated", post });
-        } catch (error) {
-          console.error(error);
+  },
+  async update(req, res) {
+    try {
+      const post = await Post.findByIdAndUpdate(
+        req.params._id,
+        { ...req.body, userId: req.user._id },
+        {
+          new: true,
         }
-      },
+      );
+      res.send({ message: "Post successfully updated", post });
+    } catch (error) {
+      console.error(error);
     }
-    
-    module.exports = PostController;
+  },
+   async delete(req, res) {
+    try {
+      const post = await Post.findByIdAndDelete(req.params._id);
+      res.send({ message: "Post deleted", post });
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({
+        message: "there was a problem trying to remove the post",
+      });
+    }
+  },
+}
+
+module.exports = PostController;
