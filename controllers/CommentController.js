f146187638c470c72962d1b267b370eec929cdb5
VolodymyrKolomiets <kolomiietsv88@gmail.com>
2023-04-27 17:02:27 +0200

    connect post id to comments

--- controllers/CommentController.js
+++ controllers/CommentController.js
@@ -1,9 +1,11 @@
 const Comment = require('../models/Comment');
+const Post = require('../models/Post')
 
 const CommentController = {
     async create(req, res) {
         try {
-            const comment = await Comment.create({...req.body, userId : req.user._id})
+            const comment = await Comment.create({...req.body, userId : req.user._id, postId : req.params._id})
+            await Post.findByIdAndUpdate(req.params._id, {$push:{ comments : comment}})
             res.status(201).send(comment)
         } catch (error) {
             console.error(error)
